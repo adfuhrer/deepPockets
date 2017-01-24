f8d4e7905c14e281c96b021868296ba17b721e39
Sammer <sammer.puran1@students.unibe.ch>
2017-01-24 20:47:29 +0100

    changed a bit the tensorflow code

--- homerun.py
+++ homerun.py
@@ -3,17 +3,17 @@ import tensorflow as tf
 import numpy as np
 
 #Data Sets
-TRAINING_FILE = "training_full2014.csv"
-TEST_FILE = "test_full2014.csv"
+TRAINING_FILE = "training_medium.csv"
+TEST_FILE = "test_medium.csv"
 
 # Load Datasets
-training_set = tf.contrib.learn.datasets.base.load_csv(filename=TRAINING_FILE, target_dtype=np.int)
-test_set = tf.contrib.learn.datasets.base.load_csv(filename=TEST_FILE, target_dtype=np.int)
+training_set = tf.contrib.learn.datasets.base.load_csv_with_header(filename=TRAINING_FILE, target_dtype=np.int,features_dtype=np.float32)
+test_set = tf.contrib.learn.datasets.base.load_csv_with_header(filename=TEST_FILE, target_dtype=np.int,features_dtype=np.float32)
 
 x_train, x_test, y_train, y_test = training_set.data, test_set.data, training_set.target, test_set.target
-
+feature_columns = [tf.contrib.layers.real_valued_column("", dimension=38)]
 # second try
-classifier = tf.contrib.learn.DNNClassifier(hidden_units=[256], n_classes=2)
+classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,hidden_units=[256], n_classes=2)
 # our first approach
 #classifier = tf.contrib.learn.DNNClassifier(hidden_units=[30, 50, 10], n_classes=2)
 print('training finished')

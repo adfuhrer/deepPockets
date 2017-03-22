47602c34a565aabbfca34c2271b8409617c54817
adfuhrer <adrian@dhcp-99-117.vpn.unibe.ch>
2017-03-22 11:26:08 +0100

    Deleting the old versions of Matlab Code to get Dataset
    I delete ancient versions of the Matlab scripts to create the
    ‘packages’-structure in the data that is then used within the neural
    networks.

--- setUpDataDir.m
+++
@@ -1,5 +0,0 @@
-
-%specify folder in which data is saved
-Datadir= '/home/sammer/Dropbox/MatlabForDeep';
-globals.Datadir=Datadir;
-globals.outPutDirectory=strcat(globals.Datadir,'/Output');
--- setup.m
+++
@@ -1,3 +0,0 @@
-setUpDataDir;
-
-addpath(genpath('./'));

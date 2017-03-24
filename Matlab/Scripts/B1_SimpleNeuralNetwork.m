221e63374eb20737327748bdf9d95ab6a1fb91a5
adfuhrer <adrian@adrians-mbp.home>
2017-03-24 12:40:44 +0100

    Finished the NN

--- Matlab/Scripts/B1_SimpleNeuralNetwork.m
+++ Matlab/Scripts/B1_SimpleNeuralNetwork.m
@@ -27,29 +27,31 @@ load('training_newWithFutureValue.mat')
 % Notice the transpose on the training Data, as the Function takes samples 
 % on columns, not lines.
 features = trainingSet(:,1:35)';
-labelUpDown = trainingSet(:,39)';
-clear trainingSet
+labelUpDown = trainingSet(:,39); % labelUp = (trainingSet(:,39)'==1);
+%labelDown = not(labelUp);
+%labelUpDown = [labelUp;labelDown];
+clear trainingSet labelUp labelDown
 
 %--------------------------------------------------------------------------
 %%                   FIRST LAYER (unsupervised)
 hiddenSize1 = 20;
 % This is basically the magic sauce that we apply...
 % Parameters are used like in the tutorial so far.
-autoenc1 = trainAutoencoder(features',hiddenSize1, ...
-    'MaxEpochs',400, ...
+autoenc1 = trainAutoencoder(features,hiddenSize1, ...
+    'MaxEpochs',100, ...
     'L2WeightRegularization',0.004, ...
     'SparsityRegularization',4, ...
     'SparsityProportion',0.15, ...
     'ScaleData', false);
 % Here, I extract the compressed representation of the data. Notice, again,
 % the transpose on the features.
-feat1 = encode(autoenc1,features');
+feat1 = encode(autoenc1,features);
 
 %%--------------------------------------------------------------------------
 %%                   SECOND LAYER (unsupervised)
 hiddenSize2 = 12;
 autoenc2 = trainAutoencoder(feat1,hiddenSize2, ...
-    'MaxEpochs',400, ...
+    'MaxEpochs',100, ...
     'L2WeightRegularization',0.002, ...
     'SparsityRegularization',4, ...
     'SparsityProportion',0.1, ...
@@ -60,5 +62,25 @@ feat2 = encode(autoenc2,feat1);
 %%--------------------------------------------------------------------------
 %%                   THIRD LAYER (supervised)
 
-softnet = trainSoftmaxLayer(feat2,labelUpDown,'MaxEpochs',400);
+softnet = trainSoftmaxLayer(feat2,labelUpDown','MaxEpochs',400);
 deepnet = stack(autoenc1,autoenc2,softnet);
+view(deepnet)
+
+%%--------------------------------------------------------------------------
+%%                   PERLIMINARY PERFORMANCE ATTRIBUTION
+% We start by loading the test set:
+
+load('test_newWithFutureValue.mat')
+% Notice the transpose on the training Data, as the Function takes samples 
+% on columns, not lines.
+featuresTest = testSet(:,1:35)';
+labelUpDownTest = testSet(:,39);%labelUpTest = (testSet(:,39)'==1);
+%labelDownTest = not(labelUpTest);
+%labelUpDownTest = [labelUpTest;labelDownTest];
+clear testSet
+
+y = deepnet(featuresTest);
+% I scale y by the prior probabilities:
+y = (y.*0.5)./(mean(labelUpDownTest));
+
+plotconfusion(labelUpDownTest',y);
